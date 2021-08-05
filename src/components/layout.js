f7d5d52af34c2e45776075e6235a6c79def34bea
Song <stalker5217@gmail.com>
2021-08-06 01:13:11 +0900

    modify header value due to pagination

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,14 @@ import { Link } from "gatsby"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
+  
+  const isPageListPath = 
+    location.pathname === rootPath || 
+    !isNaN(location.pathname.replace(rootPath, ''))
+  
   let header
 
-  if (isRootPath) {
+  if (isPageListPath) {
     header = (
       <h1 className="main-heading">
         <Link to="/">{title}</Link>
@@ -21,7 +25,7 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <div className="global-wrapper" data-is-page-list-path={isPageListPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
