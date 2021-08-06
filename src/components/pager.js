07a7af0da1a32eb626ab749cae7633f90f00c8d8
Song <stalker5217@gmail.com>
2021-08-06 10:16:04 +0900

    modify pagination

--- src/components/pager.js
+++ src/components/pager.js
@@ -18,56 +18,82 @@ const Pager = ({ pageContext }) => {
   
   return (
     <ul
-			style={{
-				display: 'flex',
-				flexWrap: 'wrap',
-				justifyContent: 'space-between',
-				alignItems: 'center',
-				listStyle: 'none',
-				padding: 0,
-			}}
+		style={{
+			display: 'flex',
+			flexWrap: 'wrap',
+			justifyContent: 'space-between',
+			alignItems: 'center',
+			listStyle: 'none',
+			padding: 0,
+		}}
+	>
+		{!isFirst && (
+			<Link 
+				to={"/"} rel="first"
+				style={{
+					textDecoration: 'none',
+					color: '#000000'
+				}}
 			>
-				{!isFirst && (
-					<Link 
-            to={prevPage} rel="prev"
-            style={{
-              color: '#000000'
-            }}
-          >
-					  ←
-					</Link>
-				)}
+				&lt;&lt;
+			</Link>
+		)}
 
-				{Array.from(pageRange, page => (
-					<li
-						key={`pagination-number-${page}`}
-						style={{
-							margin: 0,
-						}}
-						>
-						<Link
-							to={`/${page === 1 ? '' : page}`}
-							style={{
-							// padding: rhythm(1 / 4),
-							textDecoration: 'none',
-							color: page === currentPage ? '#d40000' : '#000000',
-							}}
-						>
-							{page}
-						</Link>
-					</li>
-				))}
-						
-				{!isLast && (
-					<Link 
-            to={nextPage} rel="next"
-            style={{
-              color: '#000000'
-            }}
-          >
-						→
-					</Link>
-				)}
+		{!isFirst && (
+			<Link 
+				to={prevPage} rel="prev"
+				style={{
+					textDecoration: 'none',
+					color: '#000000'
+				}}
+			>
+				&lt;
+			</Link>
+		)}
+
+		{Array.from(pageRange, page => (
+			<li
+				key={`pagination-number-${page}`}
+				style={{
+					margin: 0,
+				}}
+				>
+				<Link
+					to={`/${page === 1 ? '' : page}`}
+					style={{
+					// padding: rhythm(1 / 4),
+					textDecoration: 'none',
+					color: page === currentPage ? '#d40000' : '#000000',
+					}}
+				>
+					{page}
+				</Link>
+			</li>
+		))}
+				
+		{!isLast && (
+			<Link 
+				to={nextPage} rel="next"
+				style={{
+					textDecoration: 'none',
+					color: '#000000'
+				}}
+			>
+				&gt;
+			</Link>
+		)}
+
+		{!isLast && (
+			<Link 
+				to={`/${numPages}`} rel="last"
+				style={{
+					textDecoration: 'none',
+					color: '#000000'
+				}}
+			>
+				&gt;&gt;
+			</Link>
+		)}
     </ul>
   )
 };
