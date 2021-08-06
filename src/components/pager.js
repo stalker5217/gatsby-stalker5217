b3032580dd944eddf5df18966c5f1093f9afe8ce
Song <stalker5217@gmail.com>
2021-08-06 23:07:46 +0900

    pager refactoring

--- src/components/pager.js
+++ src/components/pager.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-// import { rhythm } from '../utils/typography'
 
 const Pager = ({ pageContext }) => {
   const { currentPage, numPages } = pageContext
@@ -17,82 +16,38 @@ const Pager = ({ pageContext }) => {
   const pageRange = Array.from( {length: showingPageCnt}, (_, i) => rangeStartPage + i + 1)
   
   return (
-    <ul
-		style={{
-			display: 'flex',
-			flexWrap: 'wrap',
-			justifyContent: 'space-between',
-			alignItems: 'center',
-			listStyle: 'none',
-			padding: 0,
-		}}
-	>
+    <ul className="page-list">
 		{!isFirst && (
-			<Link 
-				to={"/"} rel="first"
-				style={{
-					textDecoration: 'none',
-					color: '#000000'
-				}}
-			>
-				&lt;&lt;
-			</Link>
-		)}
-
-		{!isFirst && (
-			<Link 
-				to={prevPage} rel="prev"
-				style={{
-					textDecoration: 'none',
-					color: '#000000'
-				}}
-			>
-				&lt;
-			</Link>
+			<>
+				<li className="page-list-element">
+					<Link to={"/"} rel="first"> &lt;&lt; </Link>
+				</li>
+				<li className="page-list-element">
+					<Link to={prevPage} rel="prev"> &lt; </Link>
+				</li>
+			</>
 		)}
 
 		{Array.from(pageRange, page => (
-			<li
+			<li 
+				className={page === currentPage ? "page-list-element on" : "page-list-element"}
 				key={`pagination-number-${page}`}
-				style={{
-					margin: 0,
-				}}
-				>
-				<Link
-					to={`/${page === 1 ? '' : page}`}
-					style={{
-					// padding: rhythm(1 / 4),
-					textDecoration: 'none',
-					color: page === currentPage ? '#d40000' : '#000000',
-					}}
-				>
+			>
+				<Link to={`/${page === 1 ? '' : page}`}>
 					{page}
 				</Link>
 			</li>
 		))}
 				
 		{!isLast && (
-			<Link 
-				to={nextPage} rel="next"
-				style={{
-					textDecoration: 'none',
-					color: '#000000'
-				}}
-			>
-				&gt;
-			</Link>
-		)}
-
-		{!isLast && (
-			<Link 
-				to={`/${numPages}`} rel="last"
-				style={{
-					textDecoration: 'none',
-					color: '#000000'
-				}}
-			>
-				&gt;&gt;
-			</Link>
+			<>
+				<li className="page-list-element">
+					<Link to={nextPage} rel="next"> &gt; </Link>
+				</li>
+				<li className="page-list-element">
+					<Link to={`/${numPages}`} rel="last"> &gt;&gt;</Link>
+				</li>
+			</>
 		)}
     </ul>
   )
